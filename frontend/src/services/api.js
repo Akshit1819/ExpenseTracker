9ed51250230fdcdd2fa68a0a0afa3012d364a0b3
Akshit Yadav <akshityadav9899@gmail.com>
2026-06-03 04:26:00 +0530

    Connect frontend to deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "http://localhost:3000/api",
 });
 
-// Automatically attach JWT token
 API.interceptors.request.use(
   (config) => {
 
